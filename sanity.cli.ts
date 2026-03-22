ba96e36e3c4f6ac25b4d4410de21cb02fe5d8cfb
ngabodaniel1000 <ngabodaniel1000@gmail.com>
2026-03-22 22:25:07 +0200

    chore: Remove Sanity CLI deployment app ID configuration.

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -7,6 +7,6 @@ import { defineCliConfig } from 'sanity/cli'
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET
 
-export default defineCliConfig({ api: { projectId, dataset },  deployment: {
-    appId: 'psyc7ofeyipljx1g80mmarsx'  // From your successful deploy
-  } })
+export default defineCliConfig({ api: { projectId, dataset },deployment: {
+    appId: 'hrjihxyqqy9z2s220btda9hc'
+  }})
